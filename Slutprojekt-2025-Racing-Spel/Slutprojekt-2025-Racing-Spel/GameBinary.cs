737af1c11319e23a5c8455e8a1d41cb1e97d7775
VladiBoy-netizen <vladislav.mashkov@elev.ga.lbs.se>
2025-04-22 14:10:53 +0200

    Added game logic trought GameBinary.cs

---
+++ Slutprojekt-2025-Racing-Spel/Slutprojekt-2025-Racing-Spel/GameBinary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+
+namespace Slutprojekt_2025_Racing_Spel
+{
+    class GameBinary
+    {
+        public void Start()
+        {
+
+        }
+
+        public void PreUpdate()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void LateUpdate()
+        {
+
+        }
+
+        public void Draw3D()
+        {
+
+        }
+
+        public void Draw2D()
+        {
+
+        }
+
+        public void DrawGUI()
+        {
+
+        }
+
+        public void Exit()
+        {
+
+        }
+    }
+}
--- Slutprojekt-2025-Racing-Spel/Slutprojekt-2025-Racing-Spel/Program.cs
+++ Slutprojekt-2025-Racing-Spel/Slutprojekt-2025-Racing-Spel/Program.cs
@@ -3,89 +3,114 @@ using System.IO;
 using System.Runtime.InteropServices;
 using Raylib_cs;
 
-class Program
+namespace Slutprojekt_2025_Racing_Spel
 {
-    [DllImport("kernel32.dll")]
-    static extern bool AllocConsole();
-
-    [DllImport("kernel32.dll")]
-    static extern bool FreeConsole();
-
-    static bool consoleVisible = false;
-
-    // Debugging
-    public static bool isF3 = true;
-    public static List<int> avgFPS = new List<int>();
-
-    [STAThread]
-    static void Main()
+    class Program
     {
-        Raylib.SetConfigFlags(ConfigFlags.AlwaysRunWindow);
-        Raylib.InitWindow(1280, 720, "Racing Spel");
-        Raylib.SetTargetFPS(0);
-        Raylib.SetExitKey(KeyboardKey.Null);
-        Raylib.HideCursor();
+        [DllImport("kernel32.dll")]
+        static extern bool AllocConsole();
 
-        while (!Raylib.WindowShouldClose())
+        [DllImport("kernel32.dll")]
+        static extern bool FreeConsole();
+
+        static bool consoleVisible = true;
+
+        // Debugging
+        public static bool isF3 = true;
+        public static List<int> avgFPS = [];
+
+        [STAThread]
+        static void Main()
         {
-            if (Raylib.IsKeyReleased(KeyboardKey.F3))
+            Raylib.SetConfigFlags(ConfigFlags.AlwaysRunWindow);
+            Raylib.InitWindow(1280, 720, "Racing Spel");
+            Raylib.SetTargetFPS(0);
+            Raylib.SetExitKey(KeyboardKey.Null);
+            Raylib.HideCursor();
+
+            if (consoleVisible)
             {
-                isF3 = !isF3;
-                try
-                {
-                    if (isF3) Console.WriteLine("[DEBUG] F3 Menu Enabled"); else Console.WriteLine("[DEBUG] F3 Menu Disabled");
-                }
-                catch { }
+                AllocConsole();
+                RebindConsoleOutput();
+                Console.WriteLine("[CONSOLE] Console enabled.");
+                Console.Title = "[CONSOLE] Racing Spel";
             }
 
-            // Toggle console with ~ key
-            if (Raylib.IsKeyPressed(KeyboardKey.Grave))
+            GameBinary gameBinary = new();
+            gameBinary.Start();
+
+            while (!Raylib.WindowShouldClose())
             {
-                consoleVisible = !consoleVisible;
-                if (consoleVisible)
+                if (Raylib.IsKeyReleased(KeyboardKey.F3))
                 {
-                    AllocConsole();
-                    RebindConsoleOutput();
-                    Console.WriteLine("[CONSOLE] Console enabled.");
+                    isF3 = !isF3;
+                    try
+                    {
+                        if (isF3) Console.WriteLine("[DEBUG] F3 Menu Enabled"); else Console.WriteLine("[DEBUG] F3 Menu Disabled");
+                    }
+                    catch { }
                 }
-                else
+
+                // Toggle console with ~ key
+                if (Raylib.IsKeyPressed(KeyboardKey.Grave))
                 {
-                    Console.WriteLine("[CONSOLE] Disabling console...");
-                    FreeConsole();
+                    consoleVisible = !consoleVisible;
+                    if (consoleVisible)
+                    {
+                        AllocConsole();
+                        RebindConsoleOutput();
+                        Console.WriteLine("[CONSOLE] Console enabled.");
+                        Console.Title = "[CONSOLE] Racing Spel";
+                    }
+                    else
+                    {
+                        Console.WriteLine("[CONSOLE] Disabling console...");
+                        FreeConsole();
+                    }
                 }
+
+                gameBinary.PreUpdate();
+                gameBinary.Update();
+
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+
+                gameBinary.Draw3D();
+                gameBinary.Draw2D();
+                gameBinary.DrawGUI();
+
+                // Draw UI
+                if (isF3)
+                {
+                    Raylib.DrawText("FPS : " + Raylib.GetFPS().ToString(), 5, 5, 10, Color.RayWhite);
+                    float avgFPS100 = 0;
+                    try
+                    {
+                        foreach (float f in avgFPS) avgFPS100 += f;
+                        avgFPS100 /= avgFPS.Count;
+                        avgFPS100 = (float)Math.Round(avgFPS100);
+                    }
+                    catch { avgFPS100 = Raylib.GetFPS(); }
+                    Raylib.DrawText("Avg1% FPS : " + avgFPS100.ToString(), 5, 15, 10, Color.RayWhite);
+                }
+                Raylib.EndDrawing();
+
+                gameBinary.LateUpdate();
+
+                avgFPS.Add(Raylib.GetFPS());
+                if (avgFPS.Count > 100) avgFPS.RemoveAt(0);
             }
 
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.Black);
-
-            // Draw UI
-            if (isF3)
-            {
-                Raylib.DrawText("FPS : " + Raylib.GetFPS().ToString(), 5, 5, 10, Color.RayWhite);
-                float avgFPS100 = 0;
-                try
-                {
-                    foreach (float f in avgFPS) avgFPS100 += f;
-                    avgFPS100 /= avgFPS.Count;
-                    avgFPS100 = (float)Math.Round(avgFPS100);
-                }
-                catch { avgFPS100 = Raylib.GetFPS(); }
-                Raylib.DrawText("Avg1% FPS : " + avgFPS100.ToString(), 5, 15, 10, Color.RayWhite);
-            }
-            Raylib.EndDrawing();
-
-            avgFPS.Add(Raylib.GetFPS());
-            if (avgFPS.Count > 100) avgFPS.RemoveAt(0);
+            gameBinary.Exit();
+            Raylib.CloseWindow();
         }
 
-        Raylib.CloseWindow();
-    }
-
-    static void RebindConsoleOutput()
-    {
-        var stdout = Console.OpenStandardOutput();
-        var writer = new StreamWriter(stdout) { AutoFlush = true };
-        Console.SetOut(writer);
-        Console.SetError(writer);
+        static void RebindConsoleOutput()
+        {
+            var stdout = Console.OpenStandardOutput();
+            var writer = new StreamWriter(stdout) { AutoFlush = true };
+            Console.SetOut(writer);
+            Console.SetError(writer);
+        }
     }
 }
