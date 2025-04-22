08d4ffa7997d472791cb9d79698b337abb6d3059
VladiBoy-netizen <vladislav.mashkov@elev.ga.lbs.se>
2025-04-22 09:58:45 +0200

    Basic Window Setup

--- Slutprojekt-2025-Racing-Spel/Slutprojekt-2025-Racing-Spel/Program.cs
+++ Slutprojekt-2025-Racing-Spel/Slutprojekt-2025-Racing-Spel/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Raylib_cs;
+
+class Program
+{
+    static void Main()
+    {
+        Raylib.SetConfigFlags(ConfigFlags.AlwaysRunWindow | ConfigFlags.FullscreenMode);
+        Raylib.InitWindow(1280, 720, "Racing Spel");
+        Raylib.SetTargetFPS(60);
+        Raylib.SetExitKey(KeyboardKey.Null);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Black);
+            Raylib.EndDrawing();
+        }
+    }
+}
